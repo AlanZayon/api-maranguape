73053454e49abca98d16e2d83edbbff401ab4920
root <root@Rosngela.>
2025-01-03 07:43:14 -0300

    redeploy

--- src/config/redisClient.js
+++ src/config/redisClient.js
@@ -1,6 +1,11 @@
 const Redis = require('ioredis');
 
 // Conectando ao Redis
+// const redis = new Redis({
+//   host: process.env.REDIS_HOST || '127.0.0.1',
+//   port: process.env.REDIS_PORT || 6379,
+//   family: 4,
+// });
 const redis = new Redis(process.env.REDIS_URL + '?family=0');
 
 
