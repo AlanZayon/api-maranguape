ba17ca7fcf54c58770f3fb1525e0b312a1eb8643
root <root@Rosngela.>
2025-02-10 19:57:41 -0300

    tests

--- src/config/redisClient.js
+++ src/config/redisClient.js
@@ -4,10 +4,10 @@ const Redis = require('ioredis');
 const redis = process.env.REDIS_URL
   ? new Redis(process.env.REDIS_URL + '?family=0')
   : new Redis({
-      host: process.env.REDIS_HOST || '127.0.0.1',
-      port: process.env.REDIS_PORT || 6379,
-      family: 4,
-    });
+    host: process.env.REDIS_HOST || '127.0.0.1',
+    port: process.env.REDIS_PORT || 6379,
+    family: 4,
+  });
 
 
 redis.on('connect', () => {
